26dada5806df5fcc9935527b94b4224af10bd937
AJathurshan <134499315+AJathurshan@users.noreply.github.com>
2023-05-24 16:32:44 +0530

    project 1

---
+++ idle.py
@@ -0,0 +1,14 @@
+
+print('hello,world!')
+person=input('what is your name?')
+print('hello', person)
+
+age=14
+print('i am',age ,' years old')
+14
+weight=43
+print('My weight is', weight, 'kg')
+43
+height=153
+print('My height is',height, 'cm')
+153
